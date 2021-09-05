74befc691b416bd2ec89acc147658cd130ae82a8
Atul Saini <atulsaini9886@gmail.com>
2021-09-06 01:06:27 +0530

    Added Footer

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import Title from "./components/Title";
 import UploadForm from "./components/UploadForm";
 import ImageGrid from "./components/ImageGrid";
 import Modal from "./components/Modal";
+import Footer from "./components/Footer";
 
 function App() {
   const [selectedImg, setSelectedImg] = useState(null);
 
   return (
-    <div className="App">
-      <Title />
-      <UploadForm />
-      <ImageGrid setSelectedImg={setSelectedImg} />
-      {selectedImg && (
-        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
-      )}
+    <div>
+      <div className="App">
+        <div className="content">
+          <Title />
+          <UploadForm />
+          <ImageGrid setSelectedImg={setSelectedImg} />
+          {selectedImg && (
+            <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+          )}
+        </div>
+      </div>
+
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p className="remove">developed using React JS</p>
+      <p> © Firegram</p>
+    </div>
+  );
+}
